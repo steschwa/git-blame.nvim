e19ac68b93404c85c2a608f8e20a182887f17f0b
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-02-04 17:44:53 +0100

    fix: line width calculation

--- lua/git-blame/ui.lua
+++ lua/git-blame/ui.lua
@@ -96,7 +96,7 @@ function Window:open(blame)
 
 		local col_start = 0
 		for _, part in ipairs(line) do
-			local col_end = col_start + #part.text
+			local col_end = col_start + vim.api.nvim_strwidth(part.text)
 
 			if part.hl then
 				vim.api.nvim_buf_add_highlight(self.buf, -1, part.hl, row - 1, col_start, col_end)
@@ -105,7 +105,7 @@ function Window:open(blame)
 			col_start = col_end
 		end
 
-		line_width = math.max(line_width, #line_text)
+		line_width = math.max(line_width, vim.api.nvim_strwidth(line_text))
 	end
 
 	vim.bo[self.buf].modifiable = false

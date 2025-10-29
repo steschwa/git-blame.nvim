749f166da1a543d1f49e36f129f197e6e6e38a2e
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-10-29 10:25:53 +0100

    feat: validate setup options

--- lua/git-blame/config.lua
+++ lua/git-blame/config.lua
@@ -1,16 +1,30 @@
+---@param opts git-blame.SetupOpts
+local function validate_setup_opts(opts)
+	vim.validate("lines", opts.lines, "table")
+	for i, line in ipairs(opts.lines) do
+		vim.validate(string.format("lines[%d]", i), line, "table")
+
+		for j, provider in ipairs(line) do
+			vim.validate(string.format("lines[%d][%d]", i, j), provider, "function")
+		end
+	end
+
+	vim.validate("window", opts.window, "table", true)
+end
+
 ---@class git-blame.Config
 ---@field lines git-blame.Provider[][]
----@field window any
+---@field window vim.api.keyset.win_config
 local Config = {}
 
 ---@param opts git-blame.SetupOpts
 function Config:create(opts)
-	-- TODO: validate `opts`
+	validate_setup_opts(opts)
 
 	---@type git-blame.Config
 	local c = {
 		lines = opts.lines,
-		window = vim.tbl_deep_extend("keep", opts.window, {}),
+		window = opts.window or {},
 	}
 
 	return setmetatable(c, { __index = Config })
--- lua/git-blame/types.lua
+++ lua/git-blame/types.lua
@@ -4,6 +4,6 @@
 
 ---@class git-blame.SetupOpts
 ---@field lines git-blame.Provider[][]
----@field window? any
+---@field window? vim.api.keyset.win_config
 
 ---@alias git-blame.Provider fun(blame: git-blame.BlameInfo): git-blame.Part

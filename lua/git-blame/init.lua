419810b9173416a12574bb0a0ec4301b4ab23c4a
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-10-29 10:14:17 +0100

    chore: remove unused utils

--- lua/git-blame/init.lua
+++ lua/git-blame/init.lua
@@ -1,32 +1,11 @@
 local Window = require("git-blame.ui")
 local Git = require("git-blame.git")
 local Config = require("git-blame.config")
-local TableUtils = require("git-blame.utils.table")
 
 ---@class git-blame.Instance
 ---@field config git-blame.Config
 ---@field win git-blame.Window
 local M = {}
-
----@param opts git-blame.Config
----@return git-blame.Config
-function M.create_config(opts)
-	opts = opts or {}
-
-	---@type git-blame.Config
-	local config = {
-		lines = opts.lines or {},
-		window = {
-			border = vim.o.winborder,
-		},
-	}
-
-	if TableUtils.has_key(opts.window or {}, "border") then
-		config.window.border = opts.window.border
-	end
-
-	return config
-end
 
 ---@param opts git-blame.SetupOpts
 function M.setup(opts)
